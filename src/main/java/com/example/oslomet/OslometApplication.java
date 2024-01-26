fc3d71fdaaca376e18a5fd6278b6fd2c1df54fb5
sensei2412 <144007494+sensei2412@users.noreply.github.com>
2024-01-26 15:29:12 +0100

    første

---
+++ src/main/java/com/example/oslomet/OslometApplication.java
@@ -0,0 +1,13 @@
+package com.example.oslomet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslometApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OslometApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oslomet/OslometApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oslomet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslometApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
